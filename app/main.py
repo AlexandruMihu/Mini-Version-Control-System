501822b7c2de98ae94b7eda10abea4dde1252ed1
AlexandruMihu <116515331+mihutzu@users.noreply.github.com>
2025-10-05 15:40:44 +0300

    clone a repository

--- app/main.py
+++ app/main.py
@@ -4,6 +4,25 @@ import zlib
 import hashlib
 from hashlib import sha1
 import time 
+from urllib.parse import urlparse
+import urllib.request
+import struct
+from typing import List, Tuple, Dict
+
+# def get_mode(path):
+#     """Determine the mode for a given path."""
+#     if os.path.isdir(path):
+#         return "40000"  # Directory
+#     elif os.path.islink(path):
+#         return "120000"  # Symbolic link
+#     elif os.path.isfile(path):
+#         if os.access(path, os.X_OK):
+#             return "100755"  # Executable file
+#         else:
+#             return "100644"  # Regular file
+#     else:
+#         raise ValueError(f"Unknown file type for path: {path}")
+
 
 def init():
     os.mkdir(".git")
@@ -79,9 +98,9 @@ def hashObjectFile(filepath):
         blob_data = b"blob " + str(len(contents)).encode("ascii") + b"\0" + contents
         hash_value = hashlib.sha1(blob_data).hexdigest()
         compressed = zlib.compress(blob_data)
-        dir_path = f".git/objects/{hash_value[:2]}"
-        os.makedirs(dir_path, exist_ok=True)
-        with open(f"{dir_path}/{hash_value[2:]}", "wb") as f:
+        dirPath = f".git/objects/{hash_value[:2]}"
+        os.makedirs(dirPath, exist_ok=True)
+        with open(f"{dirPath}/{hash_value[2:]}", "wb") as f:
             f.write(compressed)
         return hash_value
     except FileNotFoundError:
@@ -153,8 +172,287 @@ def commitTree(arguments):
         os.mkdir(dirPath)
         with open(f"{dirPath}/{sha1Hash[2:]}","wb") as objFile:
             objFile.write(compressedData)
-    return sha1Hash
+    print(sha1Hash) 
     
+def clone():
+    remote = sys.argv[2]
+    if len(sys.argv) == 4:
+        local = sys.argv[3]
+    else:
+        parsed = urlparse(remote)
+        local = parsed.path.split("/")[-1].replace(".git","")
+        
+    os.makedirs(local)
+    os.makedirs(os.path.join(local,".git","objects"))
+    os.makedirs(os.path.join(local,".git","refs"))
+    
+    print(f"Cloning {remote} to {local}")
+    
+    caps,refs = getRefs(remote)
+    defaultBranch = caps.get("defaultBranch","refs/heads/main")
+    
+    defaultRefSha = None
+    for sha,ref in refs:
+        if ref == defaultBranch:
+            defaultRefSha = sha
+            break
+        
+    if defaultRefSha is None:
+        raise RuntimeError(f"Default branch not found: {defaultBranch}")
+    
+    print(f"Downloading {defaultBranch} ({defaultRefSha})")
+    packfile = downloadPackfile(remote, defaultRefSha)
+    writePackfile(packfile,local)
+    
+    with open(os.path.join(local,".git","HEAD"),"w") as f:
+        f.write(f"ref: {defaultBranch}\n")
+    
+    refDir = os.path.join(local,".git",os.path.dirname(defaultBranch))
+    os.makedirs(refDir,exist_ok=True)
+    with open(os.path.join(local,".git",defaultBranch),"w") as f:
+        f.write(f"{defaultRefSha}\n")
+    
+    _, commitContent = readObject(local,defaultRefSha)
+    treeSha = commitContent[5:45].decode()
+    
+    renderTree(local,local,treeSha)
+
+def getRefs(url: str) -> Tuple[Dict[str, bool | str], List[Tuple[str, str]]]:
+    
+    url = f"{url}/info/refs?service=git-upload-pack"
+
+    req = urllib.request.Request(url)
+    refs, caps = [], {}
+    with urllib.request.urlopen(req) as response:
+        lines = response.read().split(b"\n")
+
+    
+    capBytes = lines[1].split(b"\x00")[1]
+    for cap in capBytes.split(b" "):
+        if cap.startswith(b"symref=HEAD:"):
+            caps["default_branch"] = cap.split(b":")[1].decode()
+        else:
+            caps[cap.decode()] = True
+
+    for line in lines[2:]:
+        if line.startswith(b"0000"):
+            break
+
+        sha, ref_name = line.decode().split(
+            " "
+        ) 
+        refs.append((sha[4:], ref_name))  
+    return caps, refs
+
+def downloadPackfile(url: str, want_ref: str) -> bytes:
+    url = f"{url}/git-upload-pack"
+
+    body = (
+        b"0011command=fetch0001000fno-progress"
+        + f"0032want {want_ref}\n".encode()
+        + b"0009done\n0000"
+    )
+
+    headers = {
+        "Content-Type": "application/x-git-upload-pack-request",
+        "Git-Protocol": "version=2",
+    }
+
+    req = urllib.request.Request(url, data=body, headers=headers)
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+
+    packLines = []
+
+    while data:
+        lineLen = int(data[:4], 16)
+        if lineLen == 0:
+            break
+        packLines.append(data[4:lineLen])
+        data = data[lineLen:]
+
+    return b"".join(l[1:] for l in packLines[1:]) 
+
+def writePackfile(data: bytes, targetDir: str) -> None:
+    gitDir = os.path.join(targetDir, ".git")
+
+    def nextSizeType(bs: bytes) -> Tuple[str, int, bytes]:
+        ty = (bs[0] & 0b01110000) >> 4
+        typeMap = {
+            1: "commit",
+            2: "tree",
+            3: "blob",
+            4: "tag",
+            6: "ofs_delta",
+            7: "ref_delta",
+        }
+        ty = typeMap.get(ty, "unknown")
+
+        size = bs[0] & 0b00001111
+        i = 1
+        shift = 4
+        while bs[i - 1] & 0b10000000:
+            size |= (bs[i] & 0b01111111) << shift
+            shift += 7
+            i += 1
+        return ty, size, bs[i:]
+
+    def next_size(bs: bytes) -> Tuple[int, bytes]:
+        size = bs[0] & 0b01111111
+        i = 1
+        shift = 7
+        while bs[i - 1] & 0b10000000:
+            size |= (bs[i] & 0b01111111) << shift
+            shift += 7
+            i += 1
+        return size, bs[i:]
+
+    data = data[8:]
+
+    nObjects = struct.unpack("!I", data[:4])[0]
+    data = data[4:]
+
+    print(f"Processing {nObjects} objects")
+
+    objects = []  
+    remainingData = data
+
+    for _ in range(nObjects):
+        objType, _, remainingData = nextSizeType(remainingData)
+
+        if objType in ["commit", "tree", "blob", "tag"]:
+            decomp = zlib.decompressobj()
+            content = decomp.decompress(remainingData)
+            remainingData = decomp.unused_data
+            objects.append((objType, content, None))
+
+        elif objType == "ref_delta":
+            baseSha = remainingData[:20].hex()
+            remainingData = remainingData[20:]
+
+            decomp = zlib.decompressobj()
+            delta = decomp.decompress(remainingData)
+            remainingData = decomp.unused_data
+
+            objects.append(("ref_delta", delta, baseSha))
+
+    
+    processedObjects = set()  
+
+    def processObject(obj_data: Tuple[str, bytes, str | None]) -> None:
+        objType, content, baseSha = obj_data
+
+        if objType != "ref_delta":
+            
+            store = f"{objType} {len(content)}\x00".encode() + content
+            sha = hashlib.sha1(store).hexdigest()
+
+            path = os.path.join(gitDir, "objects", sha[:2])
+            os.makedirs(path, exist_ok=True)
+            with open(os.path.join(path, sha[2:]), "wb") as f:
+                f.write(zlib.compress(store))
+
+            processedObjects.add(sha)
+            return sha
+
+        else:
+            if baseSha not in processedObjects:
+                for obj in objects:
+                    if (
+                        obj[0] != "ref_delta"
+                        and hashlib.sha1(
+                            f"{obj[0]} {len(obj[1])}\x00".encode() + obj[1]
+                        ).hexdigest()
+                        == baseSha
+                    ):
+                        processObject(obj)
+                        break
+
+            with open(f"{gitDir}/objects/{baseSha[:2]}/{baseSha[2:]}", "rb") as f:
+                baseContent = zlib.decompress(f.read())
+            baseType = baseContent.split(b" ")[0].decode()
+            baseContent = baseContent.split(b"\x00", 1)[1]
+
+            delta = content
+            _, delta = next_size(delta)  
+            _, delta = next_size(delta)  
+
+            result = b""
+            while delta:
+                cmd = delta[0]
+                if cmd & 0b10000000:  
+                    pos = 1
+                    offset = 0
+                    size = 0
+
+                    for i in range(4):
+                        if cmd & (1 << i):
+                            offset |= delta[pos] << (i * 8)
+                            pos += 1
+
+                    for i in range(3):
+                        if cmd & (1 << (4 + i)):
+                            size |= delta[pos] << (i * 8)
+                            pos += 1
+
+                    result += baseContent[offset : offset + size]
+                    delta = delta[pos:]
+                else:  
+                    size = cmd
+                    result += delta[1 : size + 1]
+                    delta = delta[size + 1 :]
+
+            store = f"{baseType} {len(result)}\x00".encode() + result
+            sha = hashlib.sha1(store).hexdigest()
+
+            path = os.path.join(gitDir, "objects", sha[:2])
+            os.makedirs(path, exist_ok=True)
+            with open(os.path.join(path, sha[2:]), "wb") as f:
+                f.write(zlib.compress(store))
+
+            processedObjects.add(sha)
+            return sha
+
+    for obj in objects:
+        processObject(obj)
+
+def readObject(path: str, sha: str) -> Tuple[str, bytes]:
+    with open(f"{path}/.git/objects/{sha[:2]}/{sha[2:]}", "rb") as f:
+        data = zlib.decompress(f.read())
+
+    nullPos = data.index(b"\x00")
+    header = data[:nullPos]
+    content = data[nullPos + 1 :]
+
+    objType = header.split(b" ")[0].decode()
+    return objType, content
+
+def renderTree(repoPath: str, dirPath: str, sha: str):
+    print(f"Rendering tree {sha} to {dirPath}")
+    os.makedirs(dirPath, exist_ok=True)
+    _, treeContent = readObject(repoPath, sha)
+
+    
+    while treeContent:
+        
+        mode, treeContent = treeContent.split(b" ", 1)
+        
+        name, treeContent = treeContent.split(b"\x00", 1)
+        
+        entrySha = treeContent[:20].hex()
+        treeContent = treeContent[20:]
+
+        entryPath = os.path.join(dirPath, name.decode())
+
+        if mode == b"40000":  
+            renderTree(repoPath, entryPath, entrySha)
+        elif mode == b"100644":  
+            _, content = readObject(repoPath, entrySha)
+            with open(entryPath, "wb") as f:
+                f.write(content)
+        else:
+            raise RuntimeError(f"Unsupported mode: {mode}")
+   
 def main():
     command = sys.argv[1]
     if command == "init":
@@ -172,7 +470,10 @@ def main():
     elif command == "write-tree":
         print(writeTree("."))  
     elif command == "commit-tree":
-        print(commitTree(sys.argv))
+        result = commitTree(sys.argv)
+        print(result)
+    elif command == "clone":
+        clone()
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
